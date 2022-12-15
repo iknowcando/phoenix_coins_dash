06c4d987b869484f71e412d8e98c80091b1a69b1
Denis A <iknowcando@yandex.ru>
2022-12-15 18:30:21 +0300

    update frame app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header/Header";
+import CoinsTablePage from "./Components/CoinsTablePage/CoinsTablePage";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <CoinsTablePage/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
