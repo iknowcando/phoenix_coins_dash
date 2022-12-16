9c13cf4552eb03ce965ad26a8145afff4a328365
Denis A <iknowcando@yandex.ru>
2022-12-16 12:40:23 +0300

    refactor styles in table page

--- src/Components/CoinsTablePage/CoinsTablePage.jsx
+++ src/Components/CoinsTablePage/CoinsTablePage.jsx
@@ -1,35 +1,38 @@
 import style from './CoinsTablePage.module.css'
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {coinsAPI} from "../../API/API";
 
-
-const CoinsTablePage = (props) => {
-
-    const [state, setState] = useState([{id: 1, name: 'bitcoin'}]);
-
+const  usePrevious=(value)=>{
+    // The ref object is a generic container whose current property is mutable ...
+    // ... and can hold any value, similar to an instance property on a class
+    const ref = useRef();
+    // Store current value in ref
     useEffect(() => {
-        coinsAPI.getCoins().then(request => setState(request))
-    }, [state])
-    /*"rank": "1",
-      "symbol": "BTC",
-      "name": "Bitcoin",
-      "supply": "17193925.0000000000000000",
-      "maxSupply": "21000000.0000000000000000",
-      "marketCapUsd": "119150835874.4699281625807300",
-      "volumeUsd24Hr": "2927959461.1750323310959460",
-      "priceUsd": "6929.8217756835584756",
-      "changePercent24Hr": "-0.8101417214350335",
-      "vwap24Hr": "7175.0663247679233209"*
+        ref.current = value;
+    }, [value]); // Only re-run if value changes
+    // Return previous value (happens before update in useEffect above)
+    return ref.current;
+}
 
-     {state.map(el =>
-                <tr key={el.id}><th>{el.rank}</th> <th>{el.symbol}</th> <th>{el.name}</th> <th>{el.priceUsd}</th> <th>{el.marketCapUsd}</th> <th>{el.changePercent24Hr}</th></tr>)
-            }
-     */
+const  CoinsTablePage = (props) => {
+
+    const [state, setState] = useState([]);
+
+
+
+
+
+    const prevState = usePrevious(state)
+    useEffect(() => {
+        coinsAPI.getCoins().then(request =>
+            setState(request))
+
+    }, [state])
     return (
         <div className={style.containerPage}>
             <div className={style.presentBlock}>
             <h1>Top Cryptocurrency by Market Capitalization</h1>
-            <h2>Total Market Cap:</h2>
+            <h2>Total Market Cap:{/*рост ${prevState[0].priceUsd < state[0].priceUsd} падение ${prevState[0].priceUsd > state[0].priceUsd*/}</h2>
             </div>
             <div>
                 <div><span>prev</span></div>
@@ -62,13 +65,17 @@ const CoinsTablePage = (props) => {
             </tr>
             </thead>
             <tbody>
-            {state.map(el =>{
+            {state.map((el,i) =>{
                 let changeStatus = Math.round(el.changePercent24Hr*100)/100;
+
+                let up = (prevState[i]?.priceUsd || state[i].priceUsd)  < state[i].priceUsd
+                let low = (prevState[i]?.priceUsd || state[i].priceUsd) > state[i].priceUsd
+
                 return (<tr className={style.row} key={el.id}>
                     <th className={style.cel}>{el.rank}</th>
                     <th className={style.cel}>{el.symbol}</th>
                     <th className={style.cel}>{el.name}</th>
-                    <th className={style.cel}>{Math.round(el.marketCapUsd*100)/100}</th>
+                    <th className={`${low? style.low:null} ${up? style.up:null} ${style.cel}`}>{Math.round(el.marketCapUsd*100)/100}</th>
                     <th className={style.cel}>{Math.round(el.priceUsd*100)/100}</th>
                     <th className={style.cel}>{Math.round(el.volumeUsd24Hr*100)/100}</th>
                     <th className={style.cel}>{Math.round(el.supply*100)/100}</th>
